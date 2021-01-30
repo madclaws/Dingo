f277671b7e0a33c6b9a181f1444175f1171e9ce6
anandu <pavanan.anandu63@gmail.com>
2021-01-30 17:04:04 +0530

    adding check origin false

--- config/prod.exs
+++ config/prod.exs
@@ -58,4 +58,5 @@ config :dingo, DingoWeb.Endpoint,
   http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443, scheme: "https"],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  server: true
+  server: true,
+  check_origin: false
